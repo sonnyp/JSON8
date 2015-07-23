508f6e0a8e37576b2ffb85b54ccf85dac4d38f86
Sonny Piers <sonny@fastmail.net>
2015-07-23 18:54:32 +0200

    better forEach error

--- packages/JSON8/lib/forEach.js
+++ packages/JSON8/lib/forEach.js
@@ -4,7 +4,7 @@ var OBJECT = require('./types').OBJECT
 
 module.exports = function forEach(obj, fn, ctx) {
   if (typeof obj !== OBJECT || obj === null)
-    throw new TypeError('object must be an iterable structure')
+    throw new TypeError(obj + ' is not a structure')
 
   if (Array.isArray(obj) || (global.Map && obj instanceof Map) || (global.Set && obj instanceof Set)) {
     obj.forEach(fn, ctx)
