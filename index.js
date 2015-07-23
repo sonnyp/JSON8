81dfcbfbe8976832799c9b0ca9d824d78ceaf960
Sonny Piers <sonny@fastmail.net>
2015-07-23 19:22:25 +0200

    short index.js

--- index.js
+++ index.js
@@ -1,23 +1,20 @@
 'use strict'
 
-var types = require('./lib/types')
 var is = require('./lib/is')
+var types = require('./lib/types')
 
+module.exports.clone = require('./lib/clone')
+module.exports.equal = require('./lib/equal')
+module.exports.forEach = require('./lib/forEach')
 module.exports.is = is
-
 for (var k in is) {
   if (k.indexOf('is') === 0)
   module.exports[k] = is[k]
 }
-
+module.exports.parse = require('./lib/parse')
+module.exports.serialize = require('./lib/serialize')
+module.exports.size = require('./lib/size')
+module.exports.type = require('./lib/type')
 for (var type in types)
   module.exports[type] = types[type]
-
-module.exports.clone = require('./lib/clone')
-module.exports.type = require('./lib/type')
-module.exports.equal = require('./lib/equal')
 module.exports.valid = require('./lib/valid')
-module.exports.serialize = require('./lib/serialize')
-module.exports.parse = require('./lib/parse')
-module.exports.forEach = require('./lib/forEach')
-module.exports.size = require('./lib/size')
