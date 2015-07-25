4c8ebbb7d7be2d02f01e9b0c8f68e47c5e291b50
Sonny Piers <sonny@fastmail.net>
2015-07-25 18:33:05 +0200

    funcional programming

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ var types = require('./lib/types')
 
 module.exports.clone = require('./lib/clone')
 module.exports.equal = require('./lib/equal')
-module.exports.every = require('./lib/every')
-module.exports.forEach = require('./lib/forEach')
-module.exports.forOf = require('./lib/forOf')
-module.exports.map = require('./lib/map')
 module.exports.is = is
 for (var k in is) {
   if (k.indexOf('is') === 0)
@@ -16,9 +12,22 @@ for (var k in is) {
 }
 module.exports.parse = require('./lib/parse')
 module.exports.serialize = require('./lib/serialize')
-module.exports.size = require('./lib/size')
-module.exports.some = require('./lib/some')
 module.exports.type = require('./lib/type')
 for (var type in types)
   module.exports[type] = types[type]
 module.exports.valid = require('./lib/valid')
+
+// structures
+module.exports.every = require('./lib/every')
+module.exports.forEach = require('./lib/forEach')
+module.exports.forOf = require('./lib/forOf')
+module.exports.has = require('./lib/has')
+module.exports.map = require('./lib/map')
+module.exports.size = require('./lib/size')
+module.exports.some = require('./lib/some')
+// array
+module.exports.add = require('./lib/add')
+module.exports.remove = require('./lib/remove')
+// object
+module.exports.set = require('./lib/set')
+module.exports.unset = require('./lib/unset')
