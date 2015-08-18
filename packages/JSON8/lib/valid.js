022127c25a273d4596fdc83dd12d863eefa82e1f
Sonny Piers <sonny@fastmail.net>
2015-08-18 21:35:31 +0200

    rework valid

--- packages/JSON8/lib/valid.js
+++ packages/JSON8/lib/valid.js
@@ -21,37 +21,24 @@ module.exports = function valid(obj) {
   else if (type !== OBJECT)
     return false
 
-  var c
-  var it
+  var i, l, item
 
   if (global.Set && obj instanceof Set) {
-    c = true
     var values = obj.values()
-    while (c === true) {
-      it = values.next()
-      if (it.done === true) c = false
-      else if (!valid(it.value)) return false
-    }
+    for (i = 0, l = obj.size; i < l; i++)
+      if (!valid(values.next().value)) return false
     return true
   }
 
   if (global.Map && obj instanceof Map) {
-    c = true
     var entries = obj.entries()
-    while (c === true) {
-      it = entries.next()
-      if (it.done === true) {
-        c = false
-        continue
-      }
-      if (typeof it.value[0] !== 'string') return false
-      else if (!valid(it.value[1])) return false
+    for (i = 0, l = obj.size; i < l; i++) {
+      item = entries.next().value
+      if (typeof item[0] !== 'string') return false
+      else if (!valid(item[1])) return false
     }
     return true
   }
-
-  var i
-  var l
 
   if (Array.isArray(obj)) {
     for (i = 0, l = obj.length; i < l; i++)
@@ -60,9 +47,7 @@ module.exports = function valid(obj) {
   }
 
   var keys = Object.keys(obj)
-  for (i = 0, l = keys.length; i < l; i++) {
-    var k = keys[i]
-    if (!valid(obj[k])) return false
-  }
+  for (i = 0, l = keys.length; i < l; i++)
+    if (!valid(obj[keys[i]])) return false
   return true
 }
