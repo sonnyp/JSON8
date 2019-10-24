6751e67638d8d6ff00ade906d1cbfe9cf3ecb319
Sonny Piers <sonny@fastmail.net>
2019-10-24 20:48:46 +0200

    text-sequence: improve benchmark

--- packages/text-sequence/benchmark/index.js
+++ packages/text-sequence/benchmark/index.js
@@ -1,81 +1,37 @@
 "use strict";
 
-const { createReadStream, readFile } = require("fs");
-const promisify = require("util.promisify");
-const JSON8TextSequence = require("..");
-const JSONStream = require("JSONStream");
+const { createReadStream } = require("fs");
+
+const Benchmark = require("benchmark");
 const JSONTextSequence = require("json-text-sequence");
-const pEvent = require("p-event");
 
-const pReadFile = promisify(readFile);
+const JSON8TextSequence = require("..");
 
-const testJSON8TextSequence = async () => {
-  const stream = createReadStream(__dirname + "/data.jts");
+const suite = new Benchmark.Suite();
 
-  stream.pipe(new JSON8TextSequence.ParseStream());
-
-  const now = Date.now();
-  stream.on("end", () => {
-    console.log("json8-text-sequence", Date.now() - now, "ms");
-  });
-  return pEvent(stream, "end");
-};
-
-const testJSONTextSequence = async () => {
-  const stream = createReadStream(__dirname + "/data.jts");
-
-  stream.pipe(new JSONTextSequence.parser());
-
-  const now = Date.now();
-  stream.on("end", () => {
-    console.log("json-text-sequence", Date.now() - now, "ms");
-  });
-  return pEvent(stream, "end");
-};
-
-const testJSONStream = async () => {
-  const stream = createReadStream(__dirname + "/data.json");
-
-  stream.pipe(JSONStream.parse());
-
-  const now = Date.now();
-  stream.on("end", () => {
-    console.log("JSONStream", Date.now() - now, "ms");
-  });
-  return pEvent(stream, "end");
-};
-
-const testRequire = async () => {
-  const now = Date.now();
-  require(__dirname + "/data.json");
-  console.log("require (non streaming)", Date.now() - now, "ms");
-};
-
-const testJSONParse = async () => {
-  const now = Date.now();
-  JSON.parse(await pReadFile(__dirname + "/data.json", "utf8"));
-  console.log(
-    "fs.readFile and JSON.parse (non streaming)",
-    Date.now() - now,
-    "ms"
-  );
-};
-
-const testJSON8TextSequenceParse = async () => {
-  const now = Date.now();
-  JSON8TextSequence.parse(await pReadFile(__dirname + "/data.jts", "utf8"));
-  console.log(
-    "fs.readFile and json8-text-sequence parse (non streaming)",
-    Date.now() - now,
-    "ms"
-  );
-};
-
-(async () => {
-  await testJSONStream();
-  await testJSON8TextSequence();
-  await testJSONTextSequence();
-  await testRequire();
-  await testJSONParse();
-  await testJSON8TextSequenceParse();
-})();
+suite
+  .add(
+    "json-text-sequence",
+    deferred => {
+      const stream = createReadStream(__dirname + "/data.jts");
+      stream.pipe(new JSONTextSequence.parser());
+      stream.on("end", () => deferred.resolve());
+    },
+    { defer: true }
+  )
+  .add(
+    "json8-text-sequence",
+    deferred => {
+      const stream = createReadStream(__dirname + "/data.jts");
+      stream.pipe(new JSON8TextSequence.ParseStream());
+      stream.on("end", () => deferred.resolve());
+    },
+    { defer: true }
+  )
+  .on("cycle", function(event) {
+    console.log(String(event.target));
+  })
+  .on("complete", function() {
+    console.log("Fastest is " + this.filter("fastest").map("name"));
+  })
+  .run({ async: true });
--- packages/text-sequence/benchmark/prepare.js
+++ packages/text-sequence/benchmark/prepare.js
@@ -5,14 +5,8 @@ const { SerializeStream } = require("..");
 const request = require("request");
 const JSONStream = require("JSONStream");
 
-(async () => {
-  const writeJSON = createWriteStream(__dirname + "/data.json");
-  const writeJST = createWriteStream(__dirname + "/data.jts");
+const writeJTS = createWriteStream(__dirname + "/data.jts");
+const stream = request("http://registry.npmjs.org/browserify");
+const versions = stream.pipe(JSONStream.parse("versions.*"));
 
-  const stream = request("http://registry.npmjs.org/browserify");
-
-  const versions = stream.pipe(JSONStream.parse("versions.*"));
-
-  versions.pipe(JSONStream.stringify()).pipe(writeJSON);
-  versions.pipe(new SerializeStream()).pipe(writeJST);
-})();
+versions.pipe(new SerializeStream()).pipe(writeJTS);
