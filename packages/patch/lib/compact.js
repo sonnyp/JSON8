01eb7955e9894c15b65bdd7c8c49e3083b946de0
Victor Silva <vpcx5@yahoo.com.br>
2019-12-03 02:11:38 -0300

    Add a test for two cancelling operations

--- packages/patch/lib/compact.js
+++ packages/patch/lib/compact.js
@@ -1,6 +1,34 @@
 "use strict";
 
+function remove(operations = []) {
+  const cancellingOperation = operations.find(op => op[1] === "add");
+  if (cancellingOperation !== undefined) {
+    return cancellingOperation;
+  }
+
+  return null;
+}
+
 function compact(jsonPatch = []) {
+  const operations = {};
+
+  for (const i in jsonPatch) {
+    const { op, path, value } = jsonPatch[i];
+    if (operations[path] === undefined) {
+      operations[path] = [];
+    }
+
+    operations[path].push([i, op, value]);
+
+    if (op === "remove") {
+      const cancelling = remove(operations[path]);
+      if (cancelling) {
+        jsonPatch.splice(cancelling[0], 1);
+        jsonPatch.pop();
+      }
+    }
+  }
+
   return jsonPatch;
 }
 
