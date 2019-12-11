a1b6bbf51b5eea6b17324fb049ea282dfa2110a2
Victor Silva <vpcx5@yahoo.com.br>
2019-12-11 20:19:34 -0300

    Add a test for multiple operations and make it pass

--- packages/patch/lib/compact.js
+++ packages/patch/lib/compact.js
@@ -1,7 +1,7 @@
 "use strict";
 
 function getCancellingOperation(operations = [], pattern = RegExp()) {
-  const cancellingOperation = operations.find(op => pattern.test(op[1]));
+  const cancellingOperation = operations.find(({ op }) => pattern.test(op));
   if (cancellingOperation !== undefined) {
     return cancellingOperation;
   }
@@ -11,11 +11,11 @@ function getCancellingOperation(operations = [], pattern = RegExp()) {
 
 const Cancelling = {
   remove: {
-    compact(operations = [], jsonPatch) {
+    compact(operations = [], jsonPatch = []) {
       const cancelling = getCancellingOperation(operations, /add|replace|copy/);
       if (cancelling) {
-        jsonPatch.splice(cancelling[0], 1);
-        if (cancelling[1] !== "replace") {
+        jsonPatch.splice(jsonPatch.indexOf(cancelling), 1);
+        if (cancelling.op !== "replace") {
           jsonPatch.pop();
         }
       }
@@ -25,50 +25,47 @@ const Cancelling = {
     compact(operations = [], jsonPatch) {
       const cancelling = getCancellingOperation(operations, /remove/);
       if (cancelling) {
-        jsonPatch.splice(cancelling[0], 1);
+        jsonPatch.splice(jsonPatch.indexOf(cancelling), 1);
         jsonPatch.pop();
       }
     },
   },
   replace: {
-    compact(operations = [], jsonPatch, value) {
+    compact(operations = [], jsonPatch, { value }) {
       const cancelling = getCancellingOperation(operations, /replace|copy/);
-      if (cancelling && cancelling[2] !== value) {
-        jsonPatch.splice(cancelling[0], 1);
+      if (cancelling && cancelling.value !== value) {
+        jsonPatch.splice(jsonPatch.indexOf(cancelling), 1);
       }
     },
   },
   copy: {
-    compact(operations = [], jsonPatch, value) {
+    compact(operations = [], jsonPatch, { value }) {
       const cancelling = getCancellingOperation(operations, /replace/);
-      if (cancelling && cancelling[2] !== value) {
-        jsonPatch.splice(cancelling[0], 1);
+      if (cancelling && cancelling.value !== value) {
+        jsonPatch.splice(jsonPatch.indexOf(cancelling), 1);
       }
     },
   },
-  move: {
-    compact() {},
-  },
-  test: {
-    compact() {},
-  },
+  move: { compact() {} },
+  test: { compact() {} },
 };
 
 function compact(jsonPatch = []) {
   const operations = {};
+  const auxPatch = [];
 
-  for (const i in jsonPatch) {
-    const { op, path, value } = jsonPatch[i];
+  jsonPatch.forEach(operation => {
+    const { op, path, value } = operation;
     if (operations[path] === undefined) {
       operations[path] = [];
     }
 
-    operations[path].push([i, op, value]);
+    operations[path].push(operation);
+    auxPatch.push(operation);
+    Cancelling[op].compact(operations[path], auxPatch, { value });
+  });
 
-    Cancelling[op].compact(operations[path], jsonPatch, value);
-  }
-
-  return jsonPatch;
+  return auxPatch;
 }
 
 module.exports = compact;
